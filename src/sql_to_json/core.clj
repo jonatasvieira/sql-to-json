8ce2df6aff356198879538c5a6e1de0ed9313cde
renato.ribeiro <renato.ribeiro@synchro.com.br>
2019-06-11 17:12:15 -0300

    Inicia teste com parsers

--- src/sql_to_json/core.clj
+++ src/sql_to_json/core.clj
@@ -1,6 +1,16 @@
 (ns sql-to-json.core)
+(require '[clojure.string :as str])
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn tokenize [string] (str/split string #" "))
+
+
+;Dicionario
+(def dicionario {
+  :select (str "SELECT")
+  :all (str "*")
+})
--- test/sql_to_json/core_test.clj
+++ test/sql_to_json/core_test.clj
@@ -2,6 +2,59 @@
   (:require [clojure.test :refer :all]
             [sql-to-json.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+
+;Quebra em tokens          
+(defn tokenize [string] (str/split string #" "))
+
+;Define nome da operação
+(defn parse-operation [expressions]
+  (if (= (expressions 0) "SELECT") 
+    [(rest expressions) {:operacao :select}] 
+    (throw "Operação inválida"))
+)
+
+;extrai colunas (método para todos os cenários)
+(defn parse-columns [expressions]
+  (if (= (first (get expressions 0)) "*") 
+  [(rest expressions) (assoc expressions :campos :all)]
+  [(extract-columns expressions)]
+  )
+)
+
+;Extrai colunas até o From (colunas específicadas)
+(defn extract-columns [expressions & {:keys [vet]  :or {vet (vector)}}]  
+    (if (= (first expressions) "FROM") 
+    vet
+    (extract-columns (rest expressions) :vet (conj vet {:field (first expressions) }))
+    ) 
+)
+
+(extract-columns ["COLUNA_1," "COLUNA_2", "FROM" "TABELA_1"])  ;Teste do método
+
+;Expressão testada
+(def operacao-mais-simples  "SELECT * FROM SOMETHING;")
+
+(defn teste [& {:keys [var]  :or {var 10}}] var) ;Exemplo de parâmetro opcional
+
+
+(deftest evaluator-tokenize
+  (testing "Expressão deve ser quebrada corretamente"
+    (= (count (tokenize operacao-mais-simples)) 4)
+  )
+)
+
+(deftest operation-parser
+  (testing "Expressão som select deve retornar nó corretamente"
+    (= (count (get (parse-operation (tokenize operacao-mais-simples)) 0)) 3) ;Vetor não deve mais conter select no retorno
+    (= ((get (parse-operation (tokenize operacao-mais-simples)) 1) :campos) "*") ;Operação deve ser select
+  )
+)
+
+(deftest columns-parser
+  (testing "Coluna deve ser trazida corretamente"
+    (= (count (get (parse-columns (parse-operation (tokenize operacao-mais-simples))) 0)) 2) ;Vetor só deve conter conteúdo após FROM
+    (= ((get (parse-columns (parse-operation (tokenize operacao-mais-simples))) 1) :operacao) "SELECT") ;Coluna deve ter o identificador *
+  )
+)
