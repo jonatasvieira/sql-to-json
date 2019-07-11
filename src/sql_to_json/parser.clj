c5f23415a64bc4ec042c5777193fe87b732b2b17
renatosrib <renatosrib@gmail.com>
2019-07-11 00:17:30 -0300

    Estabiliza inner join

--- src/sql_to_json/parser.clj
+++ src/sql_to_json/parser.clj
@@ -1,102 +1,105 @@
-(ns sql-to-json.parser)
-(require '[clojure.string :as str])
-(require '[clojure.set :as cjt])
+(ns sql-to-json.parser
+  (:require [clojure.string :as str]
+            [clojure.set :as cjt]))
+;(require '[clojure.string :as str])
+;(require '[clojure.set :as cjt])
 
 
 (def conditions [">" "<" ">=" "<=" "="])
 
 (defn get-operation [condition]
-   (def matched-conditions (cjt/intersection (set (map #(str %) condition)) (set conditions)))
-    (if (= (.size matched-conditions) 1)
-      (first matched-conditions)
-      ;Created set was reversing condition(">=" was "=>")
-      (apply str (reverse matched-conditions))))
+  (def matched-conditions (cjt/intersection (set (map #(str %) condition)) (set conditions)))
+  (if (= (.size matched-conditions) 1)
+    (first matched-conditions)
+    ;Created set was reversing condition(">=" was "=>")
+    (apply str (reverse matched-conditions))))
 
 ;Quebra em tokens          
 (defn tokenize [string] (str/split string #" "))
 
-(def operandos-3-parametros {"<" :lt "<=" :le "=" :eq ">" :gt ">=" :ge "<>" :ne "IS" :nn}) ;
+(def operandos-3-parametros {"<" :lt "<=" :le "=" :eq ">" :gt ">=" :ge "<>" :ne "IS" :nn})
+
 (defn is-valid-condition [tokens]
   (and (>= (count tokens) 3) (some #(= (second tokens) %) (keys operandos-3-parametros)))
-)
+  )
 
 (defn get-tokens [state] (get state 0))
 (defn get-flat-tree [state] (get state 1))
 
-(defn remove-especial-characters [string] (str/replace string  #"[,;]" "") )
+(defn remove-especial-characters [string] (str/replace string #"[,;]" ""))
 
 ;Define nome da operação
 (defn parse-operation [state]
-  (if (= (state 0) "SELECT") 
-    [(rest state) {:operacao :select}] 
+  (if (= (state 0) "SELECT")
+    [(rest state) {:operacao :select}]
     (throw "Operação inválida"))
-)
+  )
 
 ;Extrai colunas até o From (colunas específicadas)
-(defn extract-columns [state & {:keys [vet]  :or {vet (vector)}}]  
-    (if (= (first (get-tokens state)) "FROM") 
-    [ (get-tokens state) (assoc (get-flat-tree state) :campos vet)]
-    (extract-columns [(rest (get-tokens state)) (get-flat-tree state)] 
-                    :vet (conj vet {:field (remove-especial-characters (first (get-tokens state))) }))
-    ) 
-)
+(defn extract-columns [[tokens tree] & {:keys [vet] :or {vet (vector)}}]
+  (if (= (first tokens) "FROM")
+    [tokens (assoc tree :campos vet)]
+    (extract-columns [(rest tokens) tree]
+                     :vet (conj vet {:field (remove-especial-characters (first tokens))}))
+    )
+  )
 
 ;extrai colunas (método para todos os cenários)
-(defn parse-columns [state]  
-  (if (= (first (get-tokens state)) "*") 
-  [(rest (get-tokens state)) (assoc (get-flat-tree state) :campos :all)]
-  (extract-columns state)
+(defn parse-columns [[tokens tree]]
+  (if (= (first tokens) "*")
+    [(rest tokens) (assoc tree :campos :all)]
+    (extract-columns [tokens tree])
+    )
   )
-)
 
-(defn parse-data-source [state]
-  (if (= (first (get-tokens state)) "FROM")
-    [(rest (rest (get-tokens state)))   (assoc (get-flat-tree state) :data-source (remove-especial-characters (second (get-tokens state))))]
-    (throw "Operador FROM não informado.")
+(defn parse-data-source [[tokens tree]]
+  (if (= (first tokens) "FROM")
+    [(vec (nthrest tokens 2)) (assoc tree :data-source (remove-especial-characters (second tokens)))]
+    (throw (Exception. "Operador FROM não informado."))
+    )
   )
-)
 
-(defn get-condition-value [operator value] 
+(defn get-condition-value [operator value]
   (if (= operator "IS") nil value)
-)
+  )
 
 (defn parse-condicao [tokens]
   (if (is-valid-condition tokens)
-    { :campo (first tokens) 
-      :operador (operandos-3-parametros (second tokens)) 
-      :valor (get-condition-value (second tokens) (nth tokens 2)) 
-    }
+    {:campo    (first tokens)
+     :operador (operandos-3-parametros (second tokens))
+     :valor    (get-condition-value (second tokens) (nth tokens 2))
+     }
     (throw (Exception. "Condição específicada não é válida."))
+    )
   )
-)
 
 (defn parse-inner-join-conditions [tokens]
   (loop [block tokens condicoes []]
-    (when (>= (count block) 3) 
+    (when (>= (count block) 3)
       ;(conj condicoes {:campo ("VAL_1")})
-      (if (= (nth block 4 nil) "AND" ) 
+      (if (= (nth block 3 nil) "AND")
         (recur (subvec block 4) (conj condicoes (parse-condicao block))) ;Processa condições enquanto encontrar operador "AND"
-        [ (vec (nthrest block 4)) {:join {:condicoes (conj condicoes (parse-condicao block)) }}]
+        [(vec (nthrest block 4))  {:condicoes (conj condicoes (parse-condicao block))}]
+        )
       )
     )
   )
-)
 
-(defn parse-join [state]
-  (if (not (= (second (get-tokens state)) "JOIN"))
-    [(get-tokens state) (get-flat-tree state)] ;Caso não possua join, retorna os valores sem alteração
+(defn parse-join [[tokens tree]]
+  (if (not (= (second tokens) "JOIN"))
+    [tokens tree]                                           ;Caso não possua join, retorna os valores sem alteração
     (cond
-      (not (= (nth (get-tokens state) 3) "ON")) (print (nth (get-tokens state) 3))
-      (= (first (get-tokens state))  "INNER") ((fn [exp]
-                    [
-                      (get-tokens exp)
-                      (assoc (get-flat-tree state) :join {:condicoes (get-flat-tree exp) :type :inner :table (nth (get-tokens state) 2) } )
-                    ])
-                    (parse-inner-join-conditions (subvec (get-tokens state) 4))
+      (not (= (nth tokens 3) "ON")) (print (nth tokens 3))
+      (= (first tokens) "INNER") ((fn [exp]
+                                    [
+                                     (get-tokens exp)
+                                     (assoc tree :join {:condicoes (get-flat-tree exp) :tipo :inner :tabela (nth tokens 2)})
+                                     ])
+                                   (parse-inner-join-conditions (subvec  tokens 4)) ;Faz parse somente da novo contexto após expressão ON
+                                   )
       )
     )
   )
-)
 
 ;Retorna os tres elementos passados (ex. id=10 é retornado como ["id" "=" "10"]
 (defn extract-condition [cond-stmt]
@@ -107,45 +110,45 @@
 (defn build-conditions-map [conditions]
   (def conditions-vet (extract-condition conditions))
   {:left-field (first conditions-vet) :operator (second conditions-vet) :right-field (get conditions-vet 2)})
-  
+
 
 ; extrai as condições (depois do WHERE)
 ; Supoe que o statement sql esta assim:
 ; SELECT FIELD_1, FIELD_2 FROM TABLENAME WHERE FIELD_1.ID>=10 AND ... AND ... INNER JOIN ... ORDER BY ... GROUP BY ...
-(defn parse-conditions [state]
-  (if (or (= (first (get-tokens state)) "WHERE") (= (first (get-tokens state)) "AND"))
-    (if  (contains? (get-flat-tree state) :conditions)
-      (parse-conditions [(nthrest (get-tokens state) 2) (assoc (get-flat-tree state) :conditions (conj (get (get-flat-tree state) :conditions) (build-conditions-map (second (get-tokens state)))))])
+(defn parse-conditions [[tokens tree]]
+  (if (or (= (first tokens) "WHERE") (= (first tokens) "AND"))
+    (if (contains? tree :conditions)
+      (parse-conditions [(nthrest tokens 2) (assoc tree :conditions (conj (get tree :conditions) (build-conditions-map (second tokens))))])
       ; returns one condition state
-      (parse-conditions  [(nthrest (get-tokens state) 2) (assoc (get-flat-tree state) :conditions [(build-conditions-map (second (get-tokens state)))])]))
+      (parse-conditions [(nthrest tokens 2) (assoc tree :conditions [(build-conditions-map (second tokens))])]))
     ;return original state array
-    [(get-tokens state) (get-flat-tree state)]))
+    [tokens tree]))
 
 
 (defn generate-tree [flat-tree]
-   { :select [{ :columns (flat-tree :campos) :data-source (flat-tree :data-source) :where (flat-tree :conditions)}]}
-)
+  {:select [{:columns (flat-tree :campos) :data-source (flat-tree :data-source) :where (flat-tree :conditions)}]}
+  )
 
 (defn add-item-group-params [state]
   (if (not-empty (get (get-tokens state) :group-by))
     (assoc :group-by (get (get-flat-tree :group-by)) (first (rest (get-tokens state))))
     {:group-by (conj [] (first (rest (get-tokens state))))}))
 
-(defn parse-group-by [state]
-  (cond 
-    (= (first (get-tokens state)) "GROUP")  (parse-group-by [(rest (get-tokens state)) (get-flat-tree state)])
+(defn parse-group-by [[tokens tree]]
+  (cond
+    (= (first tokens) "GROUP") (parse-group-by [(rest tokens) tree])
 
-    (= (first (get-tokens state)) "BY")  (parse-group-by [(rest (get-tokens state)) (assoc (get-flat-tree state) :group-by (add-item-group-params state))])
-      
-    :else (print "Unexpected token %s" (first (get-tokens state)))))
+    (= (first tokens) "BY") (parse-group-by [(rest tokens) (assoc tree :group-by (add-item-group-params [tokens tree]))])
+
+    :else (print "Unexpected token %s" (first tokens))))
 
 (defn parse [sql-stmt]
   (-> sql-stmt
-    tokenize
-    parse-operation
-    parse-columns
-    parse-data-source
-    parse-conditions
-    get-flat-tree
-    generate-tree
-  ))
+      tokenize
+      parse-operation
+      parse-columns
+      parse-data-source
+      parse-conditions
+      get-flat-tree
+      generate-tree
+      ))
--- test/sql_to_json/parser_test.clj
+++ test/sql_to_json/parser_test.clj
@@ -1,56 +1,56 @@
-(ns sql-to-json.parser_test
-    (:require [clojure.test :refer :all]
-    [clojure.string :as str]
-              [sql-to-json.parser :refer :all]))
+(ns sql-to-json.parser-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [sql-to-json.parser :refer :all]))
 
-(extract-columns [["COLUNA_1," "COLUNA_2", "FROM" "TABELA_1"] {:operacao :select}])  ;Teste do método
+(extract-columns [["COLUNA_1," "COLUNA_2", "FROM" "TABELA_1"] {:operacao :select}]) ;Teste do método
 
 ;Expressão testada
-(def operacao-mais-simples  "SELECT * FROM SOMETHING;")
-(def operacao-com-colunas  "SELECT CAMPO_1, CAMPO_2 FROM SOMETHING;")
+(def operacao-mais-simples "SELECT * FROM SOMETHING;")
+(def operacao-com-colunas "SELECT CAMPO_1, CAMPO_2 FROM SOMETHING;")
 (def operacao-com-join "SELECT CAMPO_1, CAMPO_2 FROM SOMETHING INNER JOIN OTHER_THING ON CAMPO_1 = CAMPO_3")
 
-(defn teste [& {:keys [var]  :or {var 10}}] var) ;Exemplo de parâmetro opcional
+(defn teste [& {:keys [var] :or {var 10}}] var)             ;Exemplo de parâmetro opcional
 
 (deftest evaluator-tokenize
-    (testing "Expressão deve ser quebrada corretamente"
-        (= (count (tokenize operacao-mais-simples)) 4)))
+  (testing "Expressão deve ser quebrada corretamente"
+    (= (count (tokenize operacao-mais-simples)) 4)))
 
 (deftest operation-parser
-    (testing "Expressão som select deve retornar nó corretamente"
+  (testing "Expressão som select deve retornar nó corretamente"
     (is (= (count (get-tokens (parse-operation (tokenize operacao-mais-simples)))) 3)) ;Vetor não deve mais conter select no retorno
     ;Operação deve ser select
     (is (= ((get-flat-tree (parse-operation (tokenize operacao-mais-simples))) :operacao) :select))))
 
 (deftest columns-parser
-(testing "Coluna deve ser trazida corretamente"
+  (testing "Coluna deve ser trazida corretamente"
     (is (= (count (get-tokens (parse-columns (parse-operation (tokenize operacao-mais-simples))))) 2)) ;Vetor só deve conter conteúdo após FROM
     (is (= ((get-flat-tree (parse-columns (parse-operation (tokenize operacao-mais-simples)))) :campos) :all)) ;Coluna deve ter o identificador *
 
-    (is (= (count (get-tokens (parse-columns (parse-operation (tokenize operacao-com-colunas))))) 2)) 
+    (is (= (count (get-tokens (parse-columns (parse-operation (tokenize operacao-com-colunas))))) 2))
     (is (= ((first ((get-flat-tree (parse-columns (parse-operation (tokenize operacao-com-colunas)))) :campos)) :field) "CAMPO_1"))
     (is (= ((second ((get-flat-tree (parse-columns (parse-operation (tokenize operacao-com-colunas)))) :campos)) :field) "CAMPO_2"))))
 
 (deftest data-source-parser
-(testing "Fonte dos dados deve ser trazida corretamente"
+  (testing "Fonte dos dados deve ser trazida corretamente"
     (is (= (count (get-tokens (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-colunas)))))) 0))
     (is (= ((second (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-colunas))))) :data-source) "SOMETHING"))))
 
-(deftest condition-parser 
-(testing "Condições devem ser parseadas corretamente"
+(deftest condition-parser
+  (testing "Condições devem ser parseadas corretamente"
     (is (= ((parse-condicao ["VAL_1" "=" "VAL_2"]) :campo) "VAL_1"))
-    (is (= ((parse-condicao ["VAL_1" "=" "VAL_2"]) :operador)  (operandos-3-parametros "=")))
-    (is (= ((parse-condicao ["VAL_1" "=" "VAL_2"]) :valor)  "VAL_2"))
+    (is (= ((parse-condicao ["VAL_1" "=" "VAL_2"]) :operador) (operandos-3-parametros "=")))
+    (is (= ((parse-condicao ["VAL_1" "=" "VAL_2"]) :valor) "VAL_2"))))
 
-))
+    
 
 (deftest join-parser
-    (testing "Join deve retornar dados na estrutura correta"
-        (is (= (-> (get-flat-tree (parse-inner-join-conditions ["VAL_1" "=" "VAL_2"])) :join :condicoes first :campo) "VAL_1"))
-        (is (= (-> (get-flat-tree (parse-inner-join-conditions ["VAL_1" "=" "VAL_2"])) :join :condicoes first :operador) :eq))
-        (is (= (-> (get-flat-tree (parse-join [["INNER" "JOIN" "TABELA_TESTE" "ON" "VAL_1" "=" "VAL_2"]  '{}])) :join :tabela) "OTHER_THING"))
-        ;(is (= (count (get-tokens (parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join))))))) 0))        
-        ;(is (= ((get-flat-tree ((parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join))))))) :join) :inner))
-        ;(is (= ((get-flat-tree (parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join))))))  :tabela) "OTHER_THING"))
-    )
-)
+  (testing "Join deve retornar dados na estrutura correta"
+    (is (= (-> (get-flat-tree (parse-inner-join-conditions ["VAL_1" "=" "VAL_2"])) :condicoes first :campo) "VAL_1"))
+    (is (= (-> (get-flat-tree (parse-inner-join-conditions ["VAL_1" "=" "VAL_2"])) :condicoes first :operador) :eq))
+    (is (= (-> (get-flat-tree (parse-join [["INNER" "JOIN" "TABELA_TESTE" "ON" "VAL_1" "=" "VAL_2"] '{}])) :join :tabela) "TABELA_TESTE"))
+    (is (= (count (get-tokens (parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join))))))) 0))
+    (is (= (-> (get-flat-tree (parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join)))))) :join :tipo) :inner))
+    (is (= (-> (get-flat-tree (parse-join (parse-data-source (parse-columns (parse-operation (tokenize operacao-com-join)))))) :join  :tabela) "OTHER_THING"))))
+    
+  
